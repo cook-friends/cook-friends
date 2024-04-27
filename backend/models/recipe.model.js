60b10973647880ec641b8ca41e59469ae569658e
Francisco Benítez <frabenrui1@alum.us.es>
2024-04-27 09:09:35 +0200

    feat: Add Recipe model #3

---
+++ backend/models/recipe.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+// Define the schema for a recipe
+const recipeSchema = new mongoose.Schema({
+    recipeName: {
+        type: String,
+        required: true,
+    },
+    ingredients: {
+        type: [
+            {
+                quantity: {
+                    type: Number,
+                    required: true,
+                },
+                unit: {
+                    type: String,
+                    enum: ["pieces", "grams", "milliliters"],
+                    required: true,
+                },
+                name: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+        required: true,
+    },
+    instructions: {
+        type: String,
+        required: true,
+    },
+    calories: {
+        type: Number,
+        required: true,
+    },
+    dietaryTags: {
+        type: [String],
+    },
+    likes: {
+        type: Number,
+        default: 0,
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+});
+
+// Create a model for the recipe schema
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+export default Recipe;
